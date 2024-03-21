6ec883809e2b04b74d08b47467153ae0fe752747
CakeAL <1742507190@qq.com>
2024-03-21 15:08:23 +0800

    config something about ele-plus

--- cm_frontend/src/main.js
+++ cm_frontend/src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+App.use(ElementPlus, { size: "small", zIndex: 3000 });
